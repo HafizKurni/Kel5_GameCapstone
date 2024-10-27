f21a5b8f1521cdc42ef642579601eb6ca9ecba20
Hafiz Muhammad Kurniawan <kurni.hafiz2002@gmail.com>
2024-10-27 11:21:28 +0700

    MoveChar

--- HorrorGame_Capstone/Assets/Programmer4(hafis)/Scripts/PlayerMovement.cs
+++ HorrorGame_Capstone/Assets/Programmer4(hafis)/Scripts/PlayerMovement.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    float hirizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    public int maxJumps =2;
+    int jumpRemaining;
+
+    [Header("Ground Check")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,45 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(hirizontalMovement * moveSpeed, rb.velocity.y);
+        GorundCheck();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        hirizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(jumpRemaining > 0)
+        {
+            if (context.performed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x,  jumpPower);
+                jumpRemaining--;
+            }
+            else if (context.canceled)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+                jumpRemaining--;
+            }
+            
+        }
+
+    }
+
+    private void GorundCheck()
+    {
+        if(Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpRemaining = maxJumps;
+        }
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
