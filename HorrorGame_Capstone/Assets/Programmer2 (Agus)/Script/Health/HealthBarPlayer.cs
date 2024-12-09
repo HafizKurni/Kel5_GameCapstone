421373c7895d0c92d6550d31288febb0f062e5d6
ARSADZ <playusercorp@gmail.com>
2024-12-09 11:33:06 +0700

    Akses Pause

--- HorrorGame_Capstone/Assets/Programmer2 (Agus)/Script/Health/HealthBarPlayer.cs
+++ HorrorGame_Capstone/Assets/Programmer2 (Agus)/Script/Health/HealthBarPlayer.cs
@@ -2,21 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Tambahkan ini untuk Scene Management
 
 public class HealthBarPlayer : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private PlayerHealth playerHealth;
     [SerializeField] private Image totalHealthBar;
-    [SerializeField] private Image CurrentHealthBar;
-    // Start is called before the first frame update
+    [SerializeField] private Image currentHealthBar;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen; // GameObject PausePanel
+    [SerializeField] private CanvasGroup pauseCanvasGroup; // CanvasGroup PausePanel
+
     void Start()
     {
         totalHealthBar.fillAmount = playerHealth.currentHealth / 10;
+        // Pastikan PausePanel dalam kondisi nonaktif di awal
+        PauseGame(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        CurrentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Toggle pause status
+            bool isPaused = pauseScreen.activeInHierarchy;
+            PauseGame(!isPaused);
+        }
+    }
+
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+
+        if (pauseCanvasGroup != null)
+        {
+            pauseCanvasGroup.alpha = status ? 1 : 0;
+            pauseCanvasGroup.interactable = status;
+            pauseCanvasGroup.blocksRaycasts = status;
+        }
+
+        Time.timeScale = status ? 0 : 1;
+    }
+
+    // Fungsi untuk memuat scene tertentu
+    public void LoadScene(string sceneName)
+    {
+        Time.timeScale = 1; // Pastikan waktu berjalan normal sebelum memuat scene baru
+        SceneManager.LoadScene(sceneName);
     }
 }
