1d0760e343c24917a39d5d103103225febf95846
ShinTaka15 <MohammadHanif002>
2024-12-12 21:32:24 +0700

    Fix conflict player attack

--- HorrorGame_Capstone/Assets/Programmer2 (Agus)/Script/Player/PlayerAttack.cs
+++ HorrorGame_Capstone/Assets/Programmer2 (Agus)/Script/Player/PlayerAttack.cs
@@ -2,67 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
-    [Header("Basic Attack")]
-    [SerializeField] public float damage;
     [SerializeField] public float utimateCD;
+    [SerializeField] private float skillCD;
+    [SerializeField] public float damage;
     [SerializeField] private float attackRange;
     [SerializeField] private LayerMask enemyLayerMask;
-    [SerializeField] private float comboAdvanceSpeed = 0.1f;
-
-    [Header("Skill Attack")]
-    [SerializeField] private float skillCD;
-    private float skillCooldownTimer = 0f;
-
-    [Header("Ultimate Attack")]
-    [SerializeField] private Transform firePoint;
-    [SerializeField] private GameObject[] slash;
-    [SerializeField] private float ultimateAnimationDuration = 1f;
-    [SerializeField] private float ultimateCD;
-    [SerializeField] private float advanceInterval = 0.2f;
-    private float ultimateCooldownTimer = 0f;
-
     private Animator anim;
     private PlayerMovement playerMovement;
     private bool IsAttacking;
     private float coldownTimer = Mathf.Infinity;
-    private PlayerInput playerInput;
+
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireBalls;
+
     public int NoOfClick = 0;
     float lastClikTime = 0;
     public float maxComboDelay = 0.9f;
 
+    // Variabel upgrade
+    [SerializeField] private float upgradeMultiplier = 1.2f; // Multiplier upgrade
+
+    // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         playerMovement = GetComponent<PlayerMovement>();
-        playerInput = new PlayerInput(); // Inisialisasi PlayerInput
-        playerInput.Player.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        skillCooldownTimer -= Time.deltaTime;
-        ultimateCooldownTimer -= Time.deltaTime;
-
         if (Time.time - lastClikTime > maxComboDelay)
         {
             NoOfClick = 0;
         }
 
-        if (playerInput.Player.CastUltimate.triggered && ultimateCooldownTimer <= 0)
+        if (Input.GetKeyDown(KeyCode.S) && coldownTimer > utimateCD)
         {
-            StartCoroutine(CastUltimate());
+            CastUltimate();
         }
 
-        if (playerInput.Player.CastSkill.triggered && skillCooldownTimer <= 0)
+        if (Input.GetKeyDown(KeyCode.A) && coldownTimer > skillCD)
         {
             CastSkill();
         }
-        if (playerInput.Player.Attack.triggered)
+
+        if (Input.GetKeyDown(KeyCode.X))
         {
             lastClikTime = Time.time;
             NoOfClick++;
@@ -70,17 +58,10 @@ public class PlayerAttack : MonoBehaviour
             {
                 anim.SetBool("Attack 1", true);
                 AudioManager.instance.PlaysSFX("MC_Sword");
-                AdvanceCombo();
             }
             NoOfClick = Mathf.Clamp(NoOfClick, 0, 3);
         }
-    }
-
-    private void AdvanceCombo()
-    {
-        // Maju perlahan ke depan
-        Vector2 moveDirection = new Vector2(Mathf.Sign(transform.localScale.x), 0);
-        transform.Translate(moveDirection * comboAdvanceSpeed);
+        coldownTimer += Time.deltaTime;
     }
 
     public void Return1()
@@ -89,7 +70,6 @@ public class PlayerAttack : MonoBehaviour
         {
             anim.SetBool("Attack 2", true);
             AudioManager.instance.PlaysSFX("MC_Sword");
-            AdvanceCombo();
         }
         else
         {
@@ -103,7 +83,6 @@ public class PlayerAttack : MonoBehaviour
         {
             anim.SetBool("Attack 3", true);
             AudioManager.instance.PlaysSFX("MC_Sword");
-            AdvanceCombo();
         }
         else
         {
@@ -118,35 +97,21 @@ public class PlayerAttack : MonoBehaviour
          anim.SetBool("Attack 3", false);
          NoOfClick = 0;
     }
-
-    private IEnumerator CastUltimate()
+    public void CastUltimate()
     {
         anim.SetTrigger("CastUlti");
         AudioManager.instance.PlaysSFX("MC_Ulti");
 
-        ultimateCooldownTimer = ultimateCD;
+        coldownTimer = 0;
 
-        // Maju setiap beberapa detik selama animasi ultimate
-        float elapsedTime = 0f;
-        while (elapsedTime < ultimateAnimationDuration)
-        {
-            AdvanceCombo();
-            elapsedTime += advanceInterval;
-            yield return new WaitForSeconds(advanceInterval);
-        }
-
-        // Summon fireball setelah animasi
-        int fireballIndex = CheckFireball();
-        if (fireballIndex >= 0)
-        {
-            slash[fireballIndex].transform.position = firePoint.position;
-            slash[fireballIndex].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
-        }
+        fireBalls[CheckFireball()].transform.position = firePoint.position;
+        fireBalls[CheckFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
     }
 
     public void Attack()
     {
         IsAttacking = true;
+        coldownTimer = 0;
 
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(firePoint.position, attackRange, enemyLayerMask);
         foreach (Collider2D enemy in hitEnemies)
@@ -159,9 +124,9 @@ public class PlayerAttack : MonoBehaviour
         anim.SetTrigger("CastSkill");
         AudioManager.instance.PlaysSFX("MC_Skill");
 
-        skillCooldownTimer = skillCD;
-    }
-
+        coldownTimer = 0;
+    }    
+    
     private void DealDamage(Collider2D enemyCollider)
     {
         EnemyHealth enemyHealth = enemyCollider.GetComponent<EnemyHealth>();
@@ -173,9 +138,9 @@ public class PlayerAttack : MonoBehaviour
 
     private int CheckFireball()
     {
-        for (int i = 0; i < slash.Length; i++)
+        for (int i = 0; i < fireBalls.Length; i++)
         {
-            if (!slash[i].activeInHierarchy)
+            if (!fireBalls[i].activeInHierarchy)
             {
                 return i;
             }
@@ -187,5 +152,19 @@ public class PlayerAttack : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(firePoint.position, attackRange);
+    }
+
+    // Fungsi untuk upgrade damage senjata
+    public void UpgradeWeapon()
+    {
+        damage *= upgradeMultiplier;
+        Debug.Log("Weapon upgraded! New damage: " + damage);
+    }
+
+    // Fungsi untuk upgrade cooldown ultimate
+    public void UpgradeUltimate()
+    {
+        utimateCD /= upgradeMultiplier; // Mengurangi cooldown
+        Debug.Log("Ultimate upgraded! New cooldown: " + utimateCD);
     }
 }
